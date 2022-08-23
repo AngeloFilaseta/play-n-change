443bdaff9abc09bf65e8de282807734484fcdda0
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-08-23 18:39:06 +0200

    re-enabled editor, removed useless newline

--- settings.js
+++ settings.js
@@ -361,7 +361,7 @@ module.exports = {
      * is not affected by this option. To disable both the editor and the admin
      * API, use either the httpRoot or httpAdminRoot properties
      */
-    disableEditor: true,
+    disableEditor: false,
 
     /** Customising the editor
      * See https://nodered.org/docs/user-guide/runtime/configuration#editor-themes
